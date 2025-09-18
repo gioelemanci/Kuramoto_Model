bba6c08fb7886ac674d19330175cfef3f4cb12f1
gioelemanci <mancinogioele6@gmail.com>
2025-09-18 13:03:26 +0200

    added .bat file for windows build and run  (not working)

---
+++ build_and_run.bat
@@ -0,0 +1,23 @@
+@echo off
+setlocal
+
+echo [1] Configuring project...
+cmake -S . -B build
+
+echo [2] Building project...
+cmake --build build
+
+echo [3] Copying SFML DLLs...
+set SFML_DLL_DIR=build\_deps\sfml-build\bin
+set BIN_DIR=build\bin
+
+copy "%SFML_DLL_DIR%\sfml-graphics-2.dll" "%BIN_DIR%" >nul
+copy "%SFML_DLL_DIR%\sfml-window-2.dll" "%BIN_DIR%" >nul
+copy "%SFML_DLL_DIR%\sfml-system-2.dll" "%BIN_DIR%" >nul
+copy "%SFML_DLL_DIR%\sfml-audio-2.dll" "%BIN_DIR%" >nul
+
+echo [4] Running simulation...
+cd %BIN_DIR%
+Kuramoto_Model.exe
+
+endlocal
